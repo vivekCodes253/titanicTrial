06a3585919484805c924d2905432aa52c9e43ef1
vivekCodes253 <38358573+vivekCodes253@users.noreply.github.com>
2018-04-13 22:15:39 +0530

    first one!

---
+++ attempt2.py
@@ -0,0 +1,102 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('train.csv')
+dataset = dataset[dataset.Embarked.notnull()]
+#mean as nan strategy for age
+dataset.fillna(dataset.mean(), inplace=True)
+
+X = dataset.iloc[:, dataset.columns!='Survived']
+X = X.iloc[:, X.columns!='Name']
+X = X.iloc[:, X.columns!='Ticket']
+X = X.iloc[:, X.columns!='Cabin']
+X = X.iloc[:, X.columns!='Parch']
+X = X.iloc[:, X.columns!='SibSp']
+X = X.iloc[:, X.columns!='Fare']
+X = X.iloc[:, X.columns!='Embarked']
+X = X.iloc[:, X.columns!='PassengerId']
+X['Family'] = dataset['Parch']+dataset['SibSp'] # Combining Parch and SibSP to one column called family
+
+Xt = X
+X = X.values
+y = dataset.iloc[:, 1].values
+
+
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+#----------------------------------RANDOM FOREST   ------------------ 75%
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+
+
+#-------------------------------------ANN-----------------------------
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'relu', input_dim = 4))
+
+# Adding the second hidden layer
+classifier.add(Dense(output_dim = 15, init = 'uniform', activation = 'sigmoid'))
+
+# Adding the third hidden layer
+classifier.add(Dense(output_dim = 3, init = 'uniform', activation = 'relu'))
+
+# Adding the fourth hidden layer
+classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'sigmoid'))
+
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+
+
+
+
+
