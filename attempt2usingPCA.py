4e307a077af4d0b8b4e46e6af1c2549593bc450f
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2018-04-19 12:55:20 +0530

    new PCA implementation 2

--- attempt2usingPCA.py
+++ attempt2usingPCA.py
@@ -7,14 +7,12 @@ dataset = pd.read_csv('train.csv')
 dataset = dataset[dataset.Embarked.notnull()]
 #mean as nan strategy for age
 dataset.fillna(dataset.mean(), inplace=True)
-
 X = dataset.iloc[:, [2,4,5,11]]
 X['Family'] = dataset['Parch']+dataset['SibSp'] # Combining Parch and SibSP to one column called family
 # Removing rows with missing embarkment values
 # onehotencoder not working well so do a  personal encoding, 2 columns for C Q , S is the extra
 X['EmbC'] = (X['Embarked']=='C')*1
 X['EmbQ'] = (X['Embarked'] == 'Q')*1
-
 
 
 X = X.iloc[:,X.columns!='Embarked']
@@ -33,6 +31,55 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
+#PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 1)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cmSVM = confusion_matrix(y_test, y_pred)
+
+print("\nSVM Results:\n")
+cmSVM
+
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green','blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+##--------------------------EXTRAS
 #----------------------------------RANDOM FOREST   ------------------ 75%
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
@@ -60,7 +107,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'relu', input_dim = 6))
+classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'relu', input_dim = 1))
 
 # Adding the second hidden layer
 #classifier.add(Dense(output_dim = 150, init = 'uniform', activation = 'sigmoid'))
@@ -94,9 +141,4 @@ cmANN = confusion_matrix(y_test, y_pred)
 
 cmANN
 cmSVM
-
-
-
-
-
 
