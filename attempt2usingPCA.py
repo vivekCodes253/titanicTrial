da0841d80e3fbab229efe9e1f5b83012091e039d
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2018-04-19 12:59:12 +0530

    new PCA implementation 2

--- attempt2usingPCA.py
+++ attempt2usingPCA.py
@@ -33,24 +33,48 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 #PCA
 from sklearn.decomposition import PCA
-pca = PCA(n_components = 1)
+pca = PCA(n_components = 2)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 explained_variance = pca.explained_variance_ratio_
 
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
-classifier.fit(X_train, y_train)
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'relu', input_dim = 2))
+
+# Adding the second hidden layer
+#classifier.add(Dense(output_dim = 150, init = 'uniform', activation = 'sigmoid'))
+
+# Adding the third hidden layer
+classifier.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu'))
+
+# Adding the fourth hidden layer
+classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'sigmoid'))
+
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
+
+# Part 3 - Making the predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cmSVM = confusion_matrix(y_test, y_pred)
-
-print("\nSVM Results:\n")
-cmSVM
 
 
 
@@ -79,66 +103,4 @@ plt.show()
 
 
 
-##--------------------------EXTRAS
-#----------------------------------RANDOM FOREST   ------------------ 75%
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cmSVM = confusion_matrix(y_test, y_pred)
-
-print("\nSVM Results:\n")
-cmSVM
-
-
-#-------------------------------------ANN-----------------------------
-
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-
-# Initialising the ANN
-classifier = Sequential()
-
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'relu', input_dim = 1))
-
-# Adding the second hidden layer
-#classifier.add(Dense(output_dim = 150, init = 'uniform', activation = 'sigmoid'))
-
-# Adding the third hidden layer
-classifier.add(Dense(output_dim = 80, init = 'uniform', activation = 'relu'))
-
-# Adding the fourth hidden layer
-classifier.add(Dense(output_dim = 12, init = 'uniform', activation = 'sigmoid'))
-
-
-# Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
-
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-
-# Part 3 - Making the predictions and evaluating the model
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
-
-# Making the Confusion Matrix
-cmANN = confusion_matrix(y_test, y_pred)
-
-
-
-cmANN
-cmSVM
 
